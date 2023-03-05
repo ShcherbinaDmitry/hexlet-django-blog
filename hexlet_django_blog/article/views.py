c0585bf73d69372a45522fc18bd432ddd5328845
ShcherbinaDmitry <shcherbinadmitry@gmail.com>
2023-03-05 13:58:15 +0300

    add templates for article app

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('article')
+    return render(request, 'article.html', context={
+        'app_name': 'article'
+    })
