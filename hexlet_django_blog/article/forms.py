c45bde20123a46b3c89cfd9caf65d2ba3c213b24
ShcherbinaDmitry <shcherbinadmitry@gmail.com>
2023-03-12 13:35:07 +0300

    add form handling

---
+++ hexlet_django_blog/article/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+
+from hexlet_django_blog.article.models import ArticleComment
+
+class ArticleCommentForm(ModelForm):
+    class Meta:
+        model = ArticleComment
+        fields = ['content']
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 
 from hexlet_django_blog.article.models import Article
+from .forms import ArticleCommentForm
 
 # Create your views here.
 class IndexView(View):
@@ -19,3 +20,16 @@ class ArticleView(View):
         return render(request, 'article/show.html', context={
             'article': article
         })
+    
+    def post(self, request, *args, **kwargs):
+        form = ArticleCommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            # comment.content = check_for_spam(form.data['content']) - process model
+            comment.save()
+
+
+class CommentArticleView(View):
+    def get(self, request, *args, **kwargs):
+        form = CommentArticleForm()
+        return render(request, 'comment.html', {'form': form})
